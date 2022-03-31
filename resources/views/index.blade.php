24fb0dcbcacc154efe162c9ca665ef02b4a274cc
katiepalumbo <palumbo.katie@gmail.com>
2022-03-31 10:47:29 +0200

    card create

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,21 @@
 @section('pageTitle', 'Lista dei film')
 
 @section('content')
-    <ul>
+
+    <div class="container">
         @foreach ($movies as $movie)
-        <li>{{$movie['id']}} - {{$movie['title']}} - {{$movie['original_title']}} - {{$movie->nationality}} - {{$movie->date}}</li>
+            <div class="product-card">
+                <h2>{{$movie['id']}} - {{$movie['title']}}</h2>
+                <h4>Original Title: {{$movie['original_title']}}</h4>
+                <p>Nationality: {{$movie->nationality}}</p>
+                <p>Release Date: {{$movie->date}}</p>
+
+            </div>
         @endforeach
-    </ul>
+
+    </div>
+
 
 @endsection
+
+
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,6 +6,7 @@
     , initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('pageTitle')</title>
+    <link rel="stylesheet" href={{asset('css/app.css')}}>
 </head>
 <body>
     @yield('content')
