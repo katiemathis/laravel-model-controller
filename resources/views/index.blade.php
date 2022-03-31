5adca2dd704885f90639f0ead5a34c4aeecb2da2
katiepalumbo <palumbo.katie@gmail.com>
2022-03-31 10:26:36 +0200

    ciclo dei dati dei film

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,3 +3,10 @@
 @section('pageTitle', 'Lista dei film')
 
 @section('content')
+    <ul>
+        @foreach ($movies as $movie)
+        <li>{{$movie['id']}} - {{$movie['title']}} - {{$movie['original_title']}} - {{$movie->nationality}} - {{$movie->date}}</li>
+        @endforeach
+    </ul>
+
+@endsection
